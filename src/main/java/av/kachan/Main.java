6f863146c609e02dc3cb63d93a229f76fb251374
Kachanok <108370860+Kachanok@users.noreply.github.com>
2023-10-26 17:45:51 +0300

    Update Main.java

--- src/main/java/av/kachan/Main.java
+++ src/main/java/av/kachan/Main.java
@@ -3,7 +3,7 @@ package av.kachan;
 public class Main {
     public static void main(String[] args) {
 
-    private static String msg = "Hello qa_guru!";   
+    private static String msg = "Hello qa_guru! Welcome back!";   
 
         System.out.println(msg());
     }
