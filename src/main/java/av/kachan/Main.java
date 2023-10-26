94b3f9a8ee10ebf4ef6b5c03161e452c3b195ab5
fediushkina_anastasiia <kachanav1009@gmail.com>
2023-10-26 17:14:20 +0300

    changed getMessage method

--- src/main/java/av/kachan/Main.java
+++ src/main/java/av/kachan/Main.java
@@ -1,14 +1,12 @@
 package av.kachan;
 
 public class Main {
-
-    private static String msg = ""Hello qa_guru!!!!"";
     public static void main(String[] args) {
 
         System.out.println(getMessage());
     }
 
     private static String getMessage() {
-        return msg;
+        return "Hello qa_guru!";
     }
 }
